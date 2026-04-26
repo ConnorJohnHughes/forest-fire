92aeb96af7e2b3c94d91b2a15f7570f9c87b7e89
Connor <connorjohnhughes@gmail.com>
2026-04-25 20:08:41 -0700

    Added tests

--- src/FireTest.java
+++ src/FireTest.java
@@ -43,8 +43,121 @@ public class FireTest {
                assertThrows(IllegalArgumentException.class, () -> {
             Fire.timeToBurn(forest, matchR, matchC);
         });
-
-
-
     }
+
+        @Test
+        public void testSmallForest(){
+
+            //act
+             char[][] forest = {
+            {'t','.','.','t'},
+            {'.','.','t','t'},
+            {'.','.','t','t'},
+            {'t','t','t','t'}
+        };
+            //arrange
+            int matchR = 1;
+            int matchC = 2;
+            //assert
+
+            int expected = 4;
+            int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+            assertEquals(expected, actual);
+
+            
+        }
+
+
+        @Test
+         public void testOneTree (){
+
+            char[][] forest ={
+                {'t'}
+            };
+
+
+            int matchR = 0;
+            int matchC = 0;
+
+            int expected = 0;
+            int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+            assertEquals(expected, actual);
+         }
+
+         @Test
+         public void testOneTreeToBurn(){
+            char[][] forest = {
+                {'.','.','.','t'},
+                {'.','t','.','t'},
+                {'.','.','.','t'},
+                {'t','t','t','t'}
+            };
+
+            int matchR = 1;
+            int matchC = 1;
+
+            int expected = 0;
+            int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+            assertEquals(expected, actual);
+         }
+
+         @Test
+         public void testBurnEverything(){
+            char[][] forest = {
+                {'t','t','t','t','t','t','t','t','t'},
+                {'t','t','t','t','t','t','t','t','t'},
+                {'t','t','t','t','t','t','t','t','t'},
+                {'t','t','t','t','t','t','t','t','t'}
+            };
+
+            int matchR = 0;
+            int matchC = 0;
+
+            int expected = 11;
+            int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+            assertEquals(expected, actual);
+
+         }
+
+         @Test
+         public void testMatchLocationOutOfBounds(){
+            char[][] forest = {
+                {'t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t'},
+                {'t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t','t'},
+
+            };
+
+            int matchR = 2;
+            int matchC = 12;
+
+            
+            assertThrows(IndexOutOfBoundsException.class, () -> {
+                Fire.timeToBurn(forest, matchR, matchC);
+             });
+         }
+
+         @Test
+         public void testOnlyDiagnolTrees(){
+            char[][] forest = {
+                {'t','.','.','.','t'},
+                {'.','t','.','t','.'},
+                {'.','.','t','.','.'},
+                {'.','t','.','t','.'},
+                {'t','.','.','.','t'}
+            };
+
+            int matchR = 2;
+            int matchC = 2;
+
+            int expected = 0;
+            int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+            assertEquals(expected, actual);
+
+         }
+
 }
